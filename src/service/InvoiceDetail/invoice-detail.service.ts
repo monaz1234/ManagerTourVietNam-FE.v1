ba22d06c42270a40414b610b97cb5c352a85c597
monaz1234 <Chomon0011@gmail.com>
2024-11-29 10:57:19 +0700

    update
    update

--- src/service/InvoiceDetail/invoice-detail.service.ts
+++ src/service/InvoiceDetail/invoice-detail.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { InvoiceDetail } from '../../interface/invoiceDetail.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InvoiceDetailService {
+  public serviceSubject: BehaviorSubject<InvoiceDetail[]> = new BehaviorSubject<InvoiceDetail[]>([]);
+  service$: Observable<InvoiceDetail[]> = this.serviceSubject.asObservable(); // Observable để các component có thể lắng nghe
+  private totalPagesSubject: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+  totalPages$: Observable<number> = this.totalPagesSubject.asObservable();
 
   constructor() { }
 }
